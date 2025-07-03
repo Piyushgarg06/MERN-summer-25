1a355843abe41da2f57141a852d8e8ae65213679
Piyushgarg06 <erpiyushgarg17@gmail.com>
2025-07-04 00:02:32 +0530

    made a react app using vite and understood the structure and flow of a react app

--- reactLearningJournal/reactDay1/src/main.jsx
+++ reactLearningJournal/reactDay1/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import NewApp from "./newApp";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NewApp />
+  </StrictMode>
+);
---
+++ reactLearningJournal/reactDay1/src/newApp.jsx
@@ -0,0 +1,5 @@
+// making a new jsx file
+const NewApp = () => {
+  return <h1>hello React</h1>;
+};
+export default NewApp;
