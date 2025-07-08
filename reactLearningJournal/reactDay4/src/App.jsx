62cd89b87dd79383e743496e946c43e54a3c5607
Piyushgarg06 <erpiyushgarg17@gmail.com>
2025-07-08 23:33:07 +0530

    learned about reconcillation, virtual DOM, tailwind basics and how to set up tailwind with vite project and props in react

---
+++ reactLearningJournal/reactDay4/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Card from "./component/card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400">Tailwind + React</h1>
+      <Card name="piyush Garg" about="I am A Developer" />
+    </>
+  );
+}
+
+export default App;
